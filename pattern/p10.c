690656f7ce7ef9884e8795640f1fd95b901bd90a
DIVYE GUPTA <147087514+divyecoder@users.noreply.github.com>
2023-12-31 09:19:16 +0530

    Add files via upload

---
+++ pattern/p10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+    int count = 0;
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= i; j++){
+            count = count + 1;
+            printf("%d",count);
+        }
+        printf("\n");
+    }
+}
