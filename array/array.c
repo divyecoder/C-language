e6b291be728fcbbca207306aa35869d902f1eb8a
DIVYE GUPTA <147087514+divyecoder@users.noreply.github.com>
2024-01-28 16:44:03 +0530

    Create array.c

---
+++ array/array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+    int marks[3];
+
+    printf("enter marks of phy: ");
+    scanf("%d",&marks[0]);
+
+    printf("enter marks of chem: ");
+    scanf("%d",&marks[1]);
+
+    printf("enter marks of math: ");
+    scanf("%d",&marks[2]);
+
+    printf("phy = %d , chem = %d , math = %d", marks[0], marks[1], marks[2]);
+}
